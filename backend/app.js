b4e7a1dde230f0810f889c2eea7efeac27731755
rak9621 <72899317+rak9621@users.noreply.github.com>
2022-03-20 21:53:41 +0530

    setCors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cookiesParser = require("cookie-parser");
 const path = require("path");
-
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
+
 app.use(express.json());
 app.use(cookiesParser());
 
