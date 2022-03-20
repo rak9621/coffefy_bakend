4e371590cff7f8843fe96aaaa7f570cf3d873d55
rak9621 <72899317+rak9621@users.noreply.github.com>
2022-03-20 18:45:15 +0530

    setup cors module

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const cookiesParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
+app.use(express.json());
 app.use(cors())
+app.use(cookiesParser());
 
 const errorMiddleware = require("./middleware/error");
 
@@ -13,8 +15,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(express.json());
-app.use(cookiesParser());
+
 
 
 
