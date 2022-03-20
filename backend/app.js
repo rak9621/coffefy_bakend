591c5e3110817ecc897ade2ebf6ba1d9fa3dab8e
rak9621 <72899317+rak9621@users.noreply.github.com>
2022-03-20 18:58:33 +0530

    setup cors -2

--- backend/app.js
+++ backend/app.js
@@ -5,23 +5,23 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(cookiesParser());
 
 
-// const corsOptions ={
-//     origin:`${process.}`, 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin:`${process.env.PORT}`, 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+ app.use(cors(corsOptions));
 
 const errorMiddleware = require("./middleware/error");
 
 //CONFIG
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//   require("dotenv").config({ path: "backend/config/config.env" });
-// }
+ if (process.env.NODE_ENV !== "PRODUCTION") {
+   require("dotenv").config({ path: "backend/config/config.env" });
+ }
 
 
 
